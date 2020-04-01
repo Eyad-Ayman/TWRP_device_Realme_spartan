5740805ace55c6dff121e0737932bf7550ed4a7e
EYAD <33518165+EYAD-ART@users.noreply.github.com>
2020-04-01 21:15:35 +0200

    Change name to spartan

--- omni_spartan.mk
+++ omni_spartan.mk
@@ -3,11 +3,11 @@ $(call inherit-product, build/target/product/embedded.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_DEVICE := Realme 3
+PRODUCT_DEVICE := spartan
 PRODUCT_NAME := omni_RMX1821
 PRODUCT_BRAND := Realme
-PRODUCT_MODEL := RMX1821
-PRODUCT_MANUFACTURER := Oppo
+PRODUCT_MODEL := Realme 3
+PRODUCT_MANUFACTURER := Realme
 
 # ADB Fix
 PRODUCT_PROPERTY_OVERRIDES += \
